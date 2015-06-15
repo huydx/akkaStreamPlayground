3909135ff897d89b2ee58a2fb2f8c247c48b83fe
dxhuy1988 <doxuanhuy@gmail.com>
2015-06-16 00:24:40 +0900

    working version

--- src/main/scala/AerospikeStreaming.scala
+++ src/main/scala/AerospikeStreaming.scala
@@ -1,25 +1,46 @@
 import akka.actor.{ Props, ActorSystem }
-import akka.stream.ActorFlowMaterializer
-import akka.stream.actor.ActorPublisher
+import akka.stream.{ ActorFlowMaterializerSettings, ActorFlowMaterializer }
+import akka.stream.actor.ActorSubscriberMessage.{ OnError, OnNext }
+import akka.stream.actor.{ WatermarkRequestStrategy, ActorSubscriber, ActorPublisher }
 import akka.stream.actor.ActorPublisherMessage._
 import akka.stream.scaladsl._
 import com.aerospike.client.{ Record, AerospikeClient }
 import com.aerospike.client.query.{ RecordSet, Statement }
 
+import scala.concurrent.Future
+
+class AESubscriber extends ActorSubscriber {
+  import context.dispatcher
+  override val requestStrategy = WatermarkRequestStrategy(4, 2)
+  def receive = {
+    case OnNext(rec: Record) =>
+      val fprint = Future { println(s"next $rec"); Thread.sleep(1000) }
+      fprint onComplete {
+        case _ => request(5)
+      }
+    case OnError(t: Throwable) => // Flow内で何かしらExceptionを吐いた場合
+      context.stop(self)
+    case OnCompleteSink(_) => println("completed")
+  }
+}
+
 class AEPublisher(rs: RecordSet) extends ActorPublisher[Record] {
   def receive = {
     case Request(_) =>
-      rs.next()
-      onNext(rs.getRecord)
-    case Cancel =>
-      println("cancelled")
-      context.stop(self)
+      if (totalDemand > 0) {
+        if (rs.next()) onNext(rs.getRecord)
+        else { onComplete() }
+      }
+    case Cancel => context.stop(self)
   }
 }
 
 object AerospikeStreaming extends App {
   implicit val actorSystem = ActorSystem()
-  implicit val flowMaterializer = ActorFlowMaterializer()
+  implicit val mat = ActorFlowMaterializer(
+    // input bufferの数でActorの立ち上がる数が決まるのでパフォーマンスに影響がでる
+    ActorFlowMaterializerSettings(actorSystem).withInputBuffer(initialSize = 4, maxSize = 4)
+  )
 
   val client = new AerospikeClient("127.0.0.1", 3000)
   val (ns, set) = ("huydx", "akkastream")
@@ -30,7 +51,7 @@ object AerospikeStreaming extends App {
 
   val recordSet = client.query(null, stmt)
   val in = Source[Record](Props(classOf[AEPublisher], recordSet))
-  val out = Sink.foreach[Record](println)
+  val out = Sink[Record](Props(classOf[AESubscriber]))
   val g = FlowGraph { implicit builder =>
     import akka.stream.scaladsl.FlowGraphImplicits._
     in ~> out
